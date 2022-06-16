38649817331d9adbe0656e5a928a78e599fbb824
Saúl Cabrera <saulecabrera@gmail.com>
2022-06-16 16:04:53 -0400

    refactor: Create macros that define functions
    This commit refactors the bench definition macros so that individual
    bench functions are created instead of inserting all the `b.iter` calls
    in a single function body

--- benches/main.rs
+++ benches/main.rs
@@ -1,59 +1,91 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use wasm_bench::VM;
 
 const BENCH_ROOT: &'static str = "sightglass/benchmarks-next/";
 
 macro_rules! def_compile_bench {
-    ($c:expr, $name:expr, $vm:expr) => {
-        let root = std::path::Path::new(BENCH_ROOT).join($name);
-        let input = std::fs::read(root.join("benchmark.wasm")).unwrap();
-        let input_size = input.len();
-        let id = criterion::BenchmarkId::new(&format!("compilation:{}", $name), format!("{} bytes", input_size));
+    ($func:ident, $name:expr) => {
+        fn $func(c: &mut Criterion) {
+            let root = std::path::Path::new(BENCH_ROOT).join($name);
+            let input = std::fs::read(root.join("benchmark.wasm")).unwrap();
+            let input_size = input.len();
+            let id = criterion::BenchmarkId::new(&format!("compile:{}", $name), format!("{} bytes", input_size));
+            let vm = wasm_bench::VM::default();
 
-        $c.bench_with_input(id, &input, |b, input| {
-            b.iter(|| {
-                $vm.compile(input).unwrap()
+            c.bench_with_input(id, &input, |b, input| {
+                b.iter(|| {
+                    vm.compile(input).unwrap()
+                });
             });
-        });
+        }
     };
 }
 
 
 macro_rules! def_exec_bench {
-    ($c:expr, $name:expr, $vm:expr) => {
-        let cwd = std::env::current_dir().unwrap();
-        let root = std::path::Path::new(BENCH_ROOT).join($name);
+    ($func:ident, $name:expr) => {
+        fn $func(c: &mut Criterion) {
+            let cwd = std::env::current_dir().unwrap();
+            let root = std::path::Path::new(BENCH_ROOT).join($name);
 
-        assert!(std::env::set_current_dir(&root).is_ok());
-        let input = std::fs::read("benchmark.wasm").unwrap();
-        let input_size = input.len();
-        let id = criterion::BenchmarkId::new(&format!("execution:{}", $name), format!("{} bytes", input_size));
-        let mut store = $vm.make_store();
-        let bytes = $vm.compile(&input).unwrap();
-        let module = $vm.deserialize(&bytes).unwrap();
+            assert!(std::env::set_current_dir(&root).is_ok());
+            let input = std::fs::read("benchmark.wasm").unwrap();
+            let input_size = input.len();
+            let id = criterion::BenchmarkId::new(&format!("execute:{}", $name), format!("{} bytes", input_size));
+            let vm = wasm_bench::VM::default();
+            let mut store = vm.make_store();
+            let bytes = vm.compile(&input).unwrap();
+            let module = vm.deserialize(&bytes).unwrap();
 
-        $c.bench_with_input(id, &module, |b, module| {
-            b.iter(|| {
-                $vm.exec(&mut store, &module).unwrap();
+            c.bench_with_input(id, &module, |b, module| {
+                b.iter(|| {
+                    vm.exec(&mut store, &module).unwrap();
+                });
             });
-        });
-        assert!(std::env::set_current_dir(&cwd).is_ok());
+            assert!(std::env::set_current_dir(&cwd).is_ok());
+        }
     };
 }
- 
-fn compilation(c: &mut Criterion) {
-    let vm: VM = Default::default();
 
-    def_compile_bench!(c, "pulldown-cmark", vm);
-    def_compile_bench!(c, "shootout-fib2", vm);
-}
 
-fn execution(c: &mut Criterion) {
-    let vm: VM = Default::default();
+def_compile_bench!(compile_pulldown_cmark, "pulldown-cmark");
+def_exec_bench!(exec_pulldown_cmark, "pulldown-cmark");
 
-    def_exec_bench!(c, "pulldown-cmark", vm);
-    def_exec_bench!(c, "shootout-fib2", vm);
-}
+def_compile_bench!(compile_bz2, "bz2");
+def_exec_bench!(exec_bz2, "bz2");
 
-criterion_group!(benches, compilation, execution);
+def_compile_bench!(compile_shootout_base64, "shootout-base64");
+def_exec_bench!(exec_shootout_base64, "shootout-base64");
+
+def_compile_bench!(compile_shootout_fib2, "shootout-fib2");
+def_exec_bench!(exec_shootout_fib2, "shootout-fib2");
+
+def_compile_bench!(compile_shootout_heapsort, "shootout-heapsort");
+def_exec_bench!(exec_shootout_heapsort, "shootout-heapsort");
+
+def_compile_bench!(compile_shootout_keccak, "shootout-keccak");
+def_exec_bench!(exec_shootout_keccak, "shootout-keccak");
+
+def_compile_bench!(compile_shootout_ed25519, "shootout-ed25519");
+def_exec_bench!(exec_shootout_ed25519, "shootout-ed25519");
+
+criterion_group!(
+    benches,
+    compile_pulldown_cmark,
+    exec_pulldown_cmark,
+    compile_bz2,
+    // There's a weird error coming from Criterion's reporting infraestructure here.
+    // An error related to too many files
+    // exec_bz2,
+    compile_shootout_base64,
+    exec_shootout_base64,
+    compile_shootout_fib2,
+    exec_shootout_fib2,
+    compile_shootout_heapsort,
+    // Exits with exit code 1
+    // exec_shootout_heapsort
+    compile_shootout_keccak,
+    exec_shootout_keccak,
+    compile_shootout_ed25519,
+    exec_shootout_ed25519
+);
 criterion_main!(benches);
