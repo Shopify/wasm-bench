9ddc834bef333db8227f013a26498571e1572353
Saúl Cabrera <saulecabrera@gmail.com>
2022-06-16 12:54:20 -0400

    feat: Add a compilation benchmark

--- benches/main.rs
+++ benches/main.rs
@@ -17,6 +17,29 @@ macro_rules! def_compile_bench {
         });
     };
 }
+
+
+macro_rules! def_exec_bench {
+    ($c:expr, $name:expr, $vm:expr) => {
+        let cwd = std::env::current_dir().unwrap();
+        let root = std::path::Path::new(BENCH_ROOT).join($name);
+
+        assert!(std::env::set_current_dir(&root).is_ok());
+        let input = std::fs::read("benchmark.wasm").unwrap();
+        let input_size = input.len();
+        let id = criterion::BenchmarkId::new(&format!("execution:{}", $name), format!("{} bytes", input_size));
+        let mut store = $vm.make_store();
+        let bytes = $vm.compile(&input).unwrap();
+        let module = $vm.deserialize(&bytes).unwrap();
+
+        $c.bench_with_input(id, &module, |b, module| {
+            b.iter(|| {
+                $vm.exec(&mut store, &module).unwrap();
+            });
+        });
+        assert!(std::env::set_current_dir(&cwd).is_ok());
+    };
+}
  
 fn compilation(c: &mut Criterion) {
     let vm: VM = Default::default();
@@ -24,5 +47,12 @@ fn compilation(c: &mut Criterion) {
     def_compile_bench!(c, "pulldown-cmark", vm);
 }
 
-criterion_group!(benches, compilation);
+fn execution(c: &mut Criterion) {
+    let vm: VM = Default::default();
+
+    def_exec_bench!(c, "pulldown-cmark", vm);
+    def_exec_bench!(c, "shootout-fib2", vm);
+}
+
+criterion_group!(benches, compilation, execution);
 criterion_main!(benches);
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,30 @@
 use wasmtime::*;
+use wasmtime_wasi::{WasiCtx, WasiCtxBuilder};
 use anyhow::Result;
+use cap_std::fs::Dir;
+
+
+pub struct Context {
+    pub wasi: WasiCtx,
+}
+
+impl Default for Context {
+    fn default() -> Self {
+        let builder = WasiCtxBuilder::new();
+        let dir = Dir::open_ambient_dir(".", cap_std::ambient_authority()).unwrap();
+        let wasi = builder
+            // Allow access to the cwd, to read benchmark inputs
+            .preopened_dir(dir, ".").unwrap()
+            .build();
+
+        Self {
+            wasi,
+        }
+    }
+}
 
 pub struct VM {
-    engine: Engine,
+    linker: Linker<Context>,
 }
 
 impl Default for VM {
@@ -12,12 +34,41 @@ impl Default for VM {
         let options = wasmtime_cli_flags::CommonOptions::parse_from_str("").unwrap();
         let config = options.config(Some(&triple.to_string())).unwrap();
         let engine = Engine::new(&config).unwrap();
-        Self { engine }
+        let mut linker = Linker::new(&engine);
+
+        wasmtime_wasi::sync::add_to_linker(&mut linker, |ctx: &mut Context| &mut ctx.wasi).unwrap();
+        
+        // Stub out imports expected by the Wasm modules 
+        // defined by sightglass
+        linker.func_wrap("bench", "start", || {}).unwrap();
+        linker.func_wrap("bench", "end", || {}).unwrap();
+        
+        Self { linker }
     }
 }
 
 impl VM {
     pub fn compile(&self, bytecode: &[u8]) -> Result<Vec<u8>> {
-        self.engine.precompile_module(bytecode)
+        self.linker.engine().precompile_module(bytecode)
+    }
+
+    pub fn deserialize(&self, bytes: &[u8]) -> Result<Module> {
+        unsafe { 
+            Module::deserialize(&self.linker.engine(), bytes)
+        }
+    }
+
+    pub fn exec(&self, store: &mut Store<Context>, module: &Module) -> Result<()> {
+        let instance = self.linker.instantiate(store.as_context_mut(), module)?;
+        let start = instance.get_typed_func::<(), (), _>(store.as_context_mut(), "_start")?;
+
+        start.call(store.as_context_mut(), ())?;
+
+        Ok(())
+    }
+
+    pub fn make_store(&self) -> Store<Context> {
+        let context = Context::default();
+        Store::new(&self.linker.engine(), context)
     }
 }
